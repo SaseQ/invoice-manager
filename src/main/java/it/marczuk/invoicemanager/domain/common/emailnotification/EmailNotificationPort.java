2999623f109fb3f7c22755d4a95931c7a8c96c23
thebartek <bmarczuk03@gmail.com>
2022-04-04 15:09:00 +0200

    App clean up

--- src/main/java/it/marczuk/invoicemanager/domain/common/emailnotification/EmailNotificationPort.java
+++
@@ -1,8 +0,0 @@
-package it.marczuk.invoicemanager.domain.common.emailnotification;
-
-import java.util.List;
-
-public interface EmailNotificationPort {
-
-    void send(List<String> recipients, String message);
-}
